c34dfe793ca31aefa53f68a3dff2bbdd1887535c
Srinivasulu Paranduru <srinivasuluparanduru@gmail.com>
2023-04-12 20:27:08 +0100

    updated

--- Devops-project1/variables.tf
+++ Devops-project1/variables.tf
@@ -3,11 +3,11 @@ variable "region" {
   default = "eu-west-1"
 }
 variable "ami_id" {
-  type = "map"
+  type = map(string)
+
   default = {
+    eu-west-1    = "ami-09dd5f12915cfb387"
     us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn75gd33"
   }
 }
 variable "instance_type" {
@@ -18,3 +18,5 @@ variable "key_name" {
   type    = "string"
   default = "ec2-demo"
 }
+
+
